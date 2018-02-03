cf84ff5a228f8b86a345e1984e71ab29f9fdfda9
bigrpg <bigrpg@163.com>
2018-02-03 23:17:07 +0800

    improve README.md

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# wkeychain 
+# WKeyChain 
+以map方式操作keychain
+
+find:group  			以key查找value，返回NSString，不存在时返回nil
+findData:group 			以key查找value，返回NSData，不存在时返回nil
+set:value:group			设置key/value对，存在时更新，不存在时添加。value=nil时删除。value类型为NSString
+setData:value:group		等同于set:value:group, value类型为NSData
+getAll:					返回所有的key/value对，value为NSString类型
+getAllData:				返回所有的key/value对，value为NSData类型	
+clear:					清空
+
+group参数必须为nil或者为keychain share list中之一
--- wkeychain/WKeyChain.h
+++ wkeychain/WKeyChain.h
@@ -18,10 +18,10 @@
 +(BOOL) setData :(NSString * __nonnull) key  value:(NSData* __nullable)     value group:(NSString* __nullable) group;
 
 //return nil means error occurs
-//{NSString,NSData}
-+(NSDictionary * __nullable) getAllData:(NSString* __nullable)  group;
 //{NSString,NSString}
 +(NSDictionary * __nullable) getAll:(NSString* __nullable)      group;
+//{NSString,NSData}
++(NSDictionary * __nullable) getAllData:(NSString* __nullable)  group;
 
 +(BOOL) clear:(NSString* __nullable) group;
 
